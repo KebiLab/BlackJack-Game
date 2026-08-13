96103174ee55bd2703a13277d10579a454daa210
KebiLab <kebi.alexandr@mail.ru>
2026-08-13 15:49:21 +0300

    add new logic

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-let firstCard = 1
-let secondCard = 1
-let sum = firstCard+secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
 let message = ""
-let isAlive = true
+let isAlive = false
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum')
 let cardsEl = document.getElementById('cards')
 
-function start(){
+function startGame(){
+    if (isAlive == false){
+        isAlive = true
+        hasBlackJack = false
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards = [firstCard, secondCard]
+        sum = firstCard + secondCard
+        renderGame()
+    }
+    else{
+        alert("Please finish this game to start a new one")
+    }
+}
+
+function renderGame(){
     sumEl.textContent = 'Sum: ' + sum
-    cardsEl.textContent = 'Cards:  ' + firstCard + ' ' + secondCard
+    cardsEl.textContent = 'Cards:  '
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + ' '
+    }
+    
     if (sum <= 20){
         message = 'Do u want to draw a new card? 🙂'
     } else if (sum === 21){
@@ -21,4 +39,29 @@ function start(){
         isAlive = false
     }
     messageEl.textContent = message
+}
+
+function getRandomCard(){
+    let randomNum = Math.floor(Math.random()*13)+1
+    if(randomNum > 10){
+        return 10
+    }
+    else if(randomNum === 1){
+        return 11
+    }
+    else{
+        return randomNum
+    }
+}
+
+function newCard(){
+    if (isAlive){
+        let card = getRandomCard()
+        sum+=card
+        cards.push(card)
+        renderGame()
+    }
+    else{
+        alert('Please Start Game')
+    }
 }

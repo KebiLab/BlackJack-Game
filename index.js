fc6aed7eef96055c0655c10fd99d98d1ed53f7a1
KebiLab <kebi.alexandr@mail.ru>
2026-08-13 11:27:03 +0300

    Add new functional and add visible logic to display

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 11
+let secondCard = 10
 let sum = firstCard+secondCard
+let hasBlackJack = false
+let message = ""
+let isAlive = true
+let messageEl = document.getElementById('message-el')
 
-if (sum < 21){
-    console.log('Do u want to draw a new card? 🙂')
-} else if (sum === 21){
-    console.log("Wohoo! You've got BlackJack 👑")
-} else if (sum > 21){
-    console.log('U lose 😭')
+function start(){
+    if (sum <= 20){
+        message = 'Do u want to draw a new card? 🙂'
+    } else if (sum === 21){
+        message = "Wohoo! You've got BlackJack 👑"
+        hasBlackJack = true
+    } else if (sum > 21){
+        message = 'U lose 😭'
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
-
